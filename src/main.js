9d62ba6f42e1b881fd789491b6ac20c521ec5bbd
Roman Skobenkov <r.skobenkov@intensa.ru>
2025-03-08 22:27:24 +0300

    Урок 1: Неидеальность Proxy (магия Vue Proxy)

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import { ref, watchEffect, computed } from "vue";
 const counterButton = document.querySelector('button#counter');
 const resetButton = document.querySelector('button#reset');
 
+// положим в переменную объект, а не значение простого типа
+const initialState = {
+    counter: 1
+}
+
 // создаём объект
-const counterState = ref(5);
-const isCounterTooBig = computed(() => counterState.value > 10);
+const counterState = ref(initialState);
+const isCounterTooBig = computed(() => counterState.value.counter > 10);
 
 // следим за его изменениями и делаем обновление представления
 watchEffect(() => {
@@ -17,7 +22,7 @@ watchEffect(() => {
 });
 
 function renderCounter() {
-    counterButton.textContent = `счётчик ${counterState.value}`;
+    counterButton.textContent = `счётчик ${counterState.value.counter}`;
 }
 
 // вынесем навешивание класса в отдельную функцию
@@ -26,13 +31,21 @@ function updateCounterColor() {
 }
 
 counterButton.addEventListener('click', () => {
-    counterState.value += 1;
+    counterState.value.counter += 1;
 });
 
 resetButton.addEventListener('click', () => {
-    counterState.value = 0;
+    counterState.value.counter = 0;
 });
 
+// тут реактивность продолжит работать,
+// потому что тут срабатывает магия Vue: внутренние объекты значений он тоже оборачивает в Proxy.
 setInterval(() => {
-    counterState.value += 1;
+    counterState.value.counter += 1;
+}, 1000);
+
+// а вто так реактивность уже сломается
+// потому что здесь мы обновим значение в обход Proxy
+setInterval(() => {
+    initialState.value += 1;
 }, 1000);
